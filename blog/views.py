d43a04345ffeb19af90dd9f9da468e73099b7a6a
Mikhail Krasovitskiy <mikhailkrasovitskiy@gmail.com>
2023-01-29 16:50:23 +0300

    first template created

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template.loader import render_to_string
 # Create your views here.
 
 
 def main_page(request):
-    return HttpResponse('<h1>Main page</h1>')
+    return render(request, 'blog/index.html')
 
 
 def posts(request):
