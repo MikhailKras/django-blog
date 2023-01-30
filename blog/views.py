834a02c3516bfc59a7023024bd291d249fc604a1
Mikhail Krasovitskiy <mikhailkrasovitskiy@gmail.com>
2023-01-30 14:15:31 +0300

    templates for blog added

--- blog/views.py
+++ blog/views.py
@@ -9,13 +9,21 @@ def main_page(request):
 
 
 def posts(request):
-    return HttpResponse('<h1>All posts</h1>')
+    return render(request, 'blog/list_detail.html')
 
 
 def get_info_about_posts_int(request, post):
-    return HttpResponse(f'Info about post with number #{post}')
+    data = {
+        'number': post
+    }
+    return render(request, 'blog/detail_by_number.html', context=data)
+    # return HttpResponse(f'Info about post with number #{post}')
 
 
 def get_info_about_posts_str(request, post):
-    return HttpResponse(f'Info about post {post}')
+    data = {
+        'name': post
+    }
+    return render(request, 'blog/detail_by_name.html', context=data)
+    # return HttpResponse(f'Info about post {post}')
 
